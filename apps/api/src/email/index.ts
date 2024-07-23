f443242065251f3a96f8971a28131e1e2253e1ba
robin-maki <8201350+robin-maki@users.noreply.github.com>
2024-07-23 11:39:43 +0000

    이메일 발송 함수 추가 (#47)

---
+++ apps/api/src/email/index.ts
@@ -0,0 +1,67 @@
+import { SendEmailCommand } from '@aws-sdk/client-ses';
+import * as aws from '../external/aws';
+import type { ComponentProps, ComponentType, SvelteComponent } from 'svelte';
+
+const sender = 'Readable <hello@rdbl.io>';
+
+type SendEmailParams<T extends SvelteComponent> = {
+  subject: string;
+  recipient: string;
+
+  template: ComponentType<T>;
+  props: ComponentProps<T>;
+};
+
+export const sendEmail = async <T extends SvelteComponent>({
+  subject,
+  recipient,
+  template,
+  props,
+}: SendEmailParams<T>) => {
+  // @ts-expect-error svelte internal
+  const { head, css, html } = template.render(props);
+
+  const body = `
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <meta charset="utf-8" />
+        ${head}
+        <style>${css.code}</style>
+      </head>
+      <body>
+        ${html}
+      </body>
+    </html>
+  `;
+
+  await aws.ses.send(
+    new SendEmailCommand({
+      Source: sender,
+      Destination: { ToAddresses: [recipient] },
+      Message: {
+        Subject: { Data: subject },
+        Body: { Html: { Data: body } },
+      },
+    }),
+  );
+};
+
+type SendTextEmailParams = {
+  subject: string;
+  recipient: string;
+  body: string;
+};
+
+export const sendTextEmail = async ({ subject, recipient, body }: SendTextEmailParams) => {
+  await aws.ses.send(
+    new SendEmailCommand({
+      Source: sender,
+      Destination: { ToAddresses: [recipient] },
+      Message: {
+        Subject: { Data: subject },
+        Body: { Text: { Data: body } },
+      },
+    }),
+  );
+};
