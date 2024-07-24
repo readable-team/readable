57ad19003af85c7e9215033f671adc7defb401eb
dolsup <8360934+dolsup@users.noreply.github.com>
2024-07-24 10:14:36 +0000

    기초적인 레이아웃 (#59)
    사이드바 포함 기초적인 대시보드 레이아웃

---
+++ apps/dashboard/src/lib/components/Sidebar.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+  import { css } from '@readable/styled-system/css';
+  import { flex } from '@readable/styled-system/patterns';
+
+  export let sitePath: string;
+</script>
+
+<aside
+  class={flex({
+    flexDirection: 'column',
+    width: '232px',
+    padding: '20px',
+    backgroundColor: 'sidebar.surface',
+  })}
+>
+  <div>사이트(메뉴버튼)</div>
+  <div>사이트 바로가기(사이드바버튼)</div>
+  <nav>
+    <ul
+      class={flex({
+        flexDirection: 'column',
+        gap: '2px',
+      })}
+    >
+      <li><a href={`${sitePath}/pages`}>페이지</a></li>
+      <li><a href={`${sitePath}/design`}>디자인</a></li>
+      <li><a href={`${sitePath}/settings`}>설정</a></li>
+    </ul>
+  </nav>
+  <div
+    class={css({
+      marginTop: 'auto',
+    })}
+  >
+    유저 프로필
+  </div>
+</aside>
--- apps/dashboard/src/routes/(default)/workspace/[workspaceId]/site/[siteId]/+layout.svelte
+++ apps/dashboard/src/routes/(default)/workspace/[workspaceId]/site/[siteId]/+layout.svelte
@@ -1,12 +1,22 @@
 <script lang="ts">
+  import { flex } from '@readable/styled-system/patterns';
+  import Sidebar from '$lib/components/Sidebar.svelte';
+
   export let data;
 </script>
 
-사이드바:
-<a href={`/workspace/${data.workspaceId}/site/${data.siteId}/pages`}>페이지</a>
-<a href={`/workspace/${data.workspaceId}/site/${data.siteId}/design`}>디자인</a>
-<a href={`/workspace/${data.workspaceId}/site/${data.siteId}/settings`}>설정</a>
-<br />
-<br />
-
-<slot />
+<div
+  class={flex({
+    height: 'screen',
+  })}
+>
+  <Sidebar sitePath={`/workspace/${data.workspaceId}/site/${data.siteId}`} />
+  <div
+    class={flex({
+      flexDirection: 'column',
+      flexGrow: 1,
+    })}
+  >
+    <slot />
+  </div>
+</div>
