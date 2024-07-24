d0f99a4cc16afcb42865d421c87bb6ffd5ffd65b
devunt <337728+devunt@users.noreply.github.com>
2024-07-24 17:00:29 +0000

    trpc subscription에 observable 이용 (#66)

--- apps/api/src/routers/playground.ts
+++ apps/api/src/routers/playground.ts
@@ -1,11 +1,18 @@
+import { observable } from '@trpc/server/observable';
 import { router, sessionProcedure } from '@/trpc';
 
 export const playgroundRouter = router({
-  stream: sessionProcedure.subscription(async function* () {
-    let i = 0;
-    while (true) {
-      yield i++;
-      await Bun.sleep(1000);
-    }
+  stream: sessionProcedure.subscription(() => {
+    return observable<number>((observer) => {
+      let i = 0;
+
+      const timer = setInterval(() => {
+        observer.next(i++);
+      }, 1000);
+
+      return () => {
+        clearInterval(timer);
+      };
+    });
   }),
 });
