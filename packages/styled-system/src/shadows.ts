b3d2e3f5500477113a73283fbb617e81d2b32ac7
Seohyun-Roh <76952602+Seohyun-Roh@users.noreply.github.com>
2024-07-29 03:34:57 +0000

    섀도우 색상 변경 (#105)

--- packages/styled-system/src/shadows.ts
+++ packages/styled-system/src/shadows.ts
@@ -2,27 +2,31 @@ import { defineTokens } from '@pandacss/dev';
 
 export const shadows = defineTokens.shadows({
   normal: {
-    value: ['0 1px 2px 0 rgba(0, 0, 0, 0.12)', '0 0px 1px 0 rgba(0, 0, 0, 0.08)', '0px 0px 1px 0 rgba(0, 0, 0, 0.08)'],
+    value: [
+      '0 1px 5px 0 rgba(80, 84, 90, 0.06)',
+      '0 0px 3px 0 rgba(80, 84, 90, 0.04)',
+      '0px 0px 7px 0 rgba(80, 84, 90, 0.04)',
+    ],
   },
   emphasize: {
     value: [
-      '0px 2px 8px 0 rgba(0, 0, 0, 0.12)',
-      '0px 1px 4px 0 rgba(0, 0, 0, 0.08)',
-      '0px 0px 1px 0 rgba(0, 0, 0, 0.08)',
+      '0px 2px 8px 0 rgba(80, 84, 90, 0.12)',
+      '0px 1px 4px 0 rgba(80, 84, 90, 0.08)',
+      '0px 0px 1px 0 rgba(80, 84, 90, 0.08)',
     ],
   },
   strong: {
     value: [
-      '0px 6px 12px 0 rgba(0, 0, 0, 0.12)',
-      '0px 4px 8px 0 rgba(0, 0, 0, 0.08)',
-      '0px 0px 4px 0 rgba(0, 0, 0, 0.08)',
+      '0px 6px 12px 0 rgba(80, 84, 90, 0.12)',
+      '0px 4px 8px 0 rgba(80, 84, 90, 0.08)',
+      '0px 0px 4px 0 rgba(80, 84, 90, 0.08)',
     ],
   },
   heavy: {
     value: [
-      '0px 16px 20px rgba(0, 0, 0, 0.12)',
-      '0px 8px 16px 0 rgba(0, 0, 0, 0.08)',
-      '0px 0px 8px 0 rgba(0, 0, 0, 0.08)',
+      '0px 16px 20px rgba(80, 84, 90, 0.12)',
+      '0px 8px 16px 0 rgba(80, 84, 90, 0.08)',
+      '0px 0px 8px 0 rgba(80, 84, 90, 0.08)',
     ],
   },
 });
