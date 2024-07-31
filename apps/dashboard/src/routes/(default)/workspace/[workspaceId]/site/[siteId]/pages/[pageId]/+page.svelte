5729f1cb4e647fa683af89345afb026eaaa62fae
dolsup <8360934+dolsup@users.noreply.github.com>
2024-07-31 09:55:51 +0000

    에디팅 페이지 하단 플로팅 메뉴 퍼블리싱 (#163)

--- apps/dashboard/src/routes/(default)/workspace/[workspaceId]/site/[siteId]/pages/[pageId]/+page.svelte
+++ apps/dashboard/src/routes/(default)/workspace/[workspaceId]/site/[siteId]/pages/[pageId]/+page.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
   import { css } from '@readable/styled-system/css';
+  import { flex } from '@readable/styled-system/patterns';
+  import { Button, Icon } from '@readable/ui/components';
   import { TiptapEditor } from '@readable/ui/tiptap';
   import { fromUint8Array, toUint8Array } from 'js-base64';
   import { nanoid } from 'nanoid';
@@ -7,6 +9,8 @@
   import * as YAwareness from 'y-protocols/awareness';
   import * as Y from 'yjs';
   import { PageContentSyncKind } from '@/enums';
+  import ClockIcon from '~icons/lucide/clock';
+  import EllipsisIcon from '~icons/lucide/ellipsis';
   import { graphql } from '$graphql';
 
   $: query = graphql(`
@@ -162,4 +166,77 @@
 
 <div class={css({ margin: '100px', borderWidth: '4px', padding: '20px' })}>
   <TiptapEditor style={css.raw({ height: '[2000px]' })} awareness={yAwareness} document={yDoc} />
+</div>
+
+<div
+  class={flex({
+    position: 'sticky',
+    bottom: '56px',
+    marginX: 'auto',
+    width: '[fit-content]',
+    height: '58px',
+    borderRadius: '12px',
+    backgroundColor: { base: 'white', _dark: 'darkgray.700' }, // FIXME: surface.tertiary
+    alignItems: 'center',
+    shadow: 'strong',
+  })}
+>
+  <div
+    class={flex({
+      gap: '8px',
+      paddingX: '12px',
+      paddingY: '10px',
+    })}
+  >
+    <Button
+      style={css.raw({
+        paddingX: '9px',
+      })}
+      variant="secondary"
+    >
+      <Icon icon={ClockIcon} size={18} />
+    </Button>
+    <Button
+      style={css.raw({
+        paddingX: '9px',
+      })}
+      variant="secondary"
+    >
+      <Icon icon={EllipsisIcon} size={18} />
+    </Button>
+  </div>
+
+  <hr
+    class={css({
+      borderColor: 'divider.secondary',
+      borderLeftWidth: '1px',
+      height: 'full',
+    })}
+  />
+
+  <div
+    class={flex({
+      paddingX: '12px',
+      alignItems: 'center',
+      gap: '10px',
+    })}
+  >
+    <span
+      class={css({
+        color: 'text.tertiary',
+        textStyle: '14sb',
+      })}
+    >
+      2일 전 발행됨
+    </span>
+
+    <div
+      class={flex({
+        gap: '8px',
+      })}
+    >
+      <Button variant="secondary">발행취소</Button>
+      <Button>발행</Button>
+    </div>
+  </div>
 </div>
