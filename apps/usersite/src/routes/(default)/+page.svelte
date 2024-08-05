148ce73914711223ef990a00e6a598a756e7e480
dolsup <8360934+dolsup@users.noreply.github.com>
2024-08-05 09:17:11 +0000

    유저사이트에서 모든 페이지 트리 표시 (#199)
    일단 테스트를 위해!

--- apps/usersite/src/routes/(default)/+page.svelte
+++ apps/usersite/src/routes/(default)/+page.svelte
@@ -8,6 +8,7 @@
         id
         name
 
+        # NOTE: maxDepth = 3
         pages {
           id
           slug
@@ -15,6 +16,52 @@
           content {
             id
             title
+          }
+
+          parent {
+            id
+          }
+
+          children {
+            id
+            slug
+
+            content {
+              id
+              title
+            }
+
+            parent {
+              id
+            }
+
+            children {
+              id
+              slug
+
+              content {
+                id
+                title
+              }
+
+              parent {
+                id
+              }
+
+              children {
+                id
+                slug
+
+                content {
+                  id
+                  title
+                }
+
+                parent {
+                  id
+                }
+              }
+            }
           }
         }
       }
@@ -32,5 +79,32 @@ pages:
 <ul>
   {#each $query.publicSite.pages as page (page.id)}
     <li><a href={pageUrl(page)}>{page.content.title}</a></li>
+    {#if page.children}
+      <ul>
+        {#each page.children as child (child.id)}
+          <li style="margin-left: 1rem;">
+            <a href={pageUrl(child)}>{child.content.title}</a>
+          </li>
+          {#if child.children}
+            <ul>
+              {#each child.children as grandchild (grandchild.id)}
+                <li style="margin-left: 1rem;">
+                  <a href={pageUrl(grandchild)}>{grandchild.content.title}</a>
+                </li>
+                {#if grandchild.children}
+                  <ul>
+                    {#each grandchild.children as greatgrandchild (greatgrandchild.id)}
+                      <li style="margin-left: 1rem;">
+                        <a href={pageUrl(greatgrandchild)}>{greatgrandchild.content.title}</a>
+                      </li>
+                    {/each}
+                  </ul>
+                {/if}
+              {/each}
+            </ul>
+          {/if}
+        {/each}
+      </ul>
+    {/if}
   {/each}
 </ul>
