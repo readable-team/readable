b9e3116ab2bb01a7a7909c374eb4cf83e6f5e705
devunt <337728+devunt@users.noreply.github.com>
2024-08-16 13:11:07 +0000

    백스페이스 동작 좀 더 노션에 가깝게 고침 (#390)

--- packages/ui/src/tiptap/extensions/behavior.ts
+++ packages/ui/src/tiptap/extensions/behavior.ts
@@ -7,20 +7,25 @@ export const Behavior = Extension.create({
   addKeyboardShortcuts() {
     return {
       Backspace: ({ editor }) => {
-        const { selection } = editor.state;
+        const { doc, selection } = editor.state;
         const { $anchor, empty } = selection;
 
-        if (empty && $anchor.parent.isTextblock && $anchor.parent.childCount === 0 && $anchor.parentOffset === 0) {
-          const pos = $anchor.before(1);
-          const block = $anchor.node(1);
+        const pos = $anchor.before(1);
+        const block = $anchor.node(1);
 
-          if (!['paragraph', 'bulletList', 'orderedList'].includes(block.type.name)) {
+        if (empty && $anchor.parent.isTextblock && $anchor.parent.childCount === 0 && $anchor.parentOffset === 0) {
+          if (!['paragraph', 'bulletList', 'orderedList'].includes(block.type.name) && block.childCount === 0) {
             return editor
               .chain()
               .setNodeSelection(pos)
               .deleteSelection()
               .insertContentAt(pos, { type: 'paragraph' })
               .run();
+          }
+
+          const blockBefore = doc.childBefore(pos).node;
+          if (block.childCount === 0 && blockBefore?.isTextblock && blockBefore.childCount === 0) {
+            return editor.chain().setNodeSelection(pos).deleteSelection().run();
           }
         }
 
