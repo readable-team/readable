80d6b12188f1b88c1839c3f20cf2937e13f493b2
Seohyun-Roh <76952602+Seohyun-Roh@users.noreply.github.com>
2024-08-16 03:39:07 +0000

    이미지, 파일 노드뷰 디자인 반영 (#364)

--- packages/ui/src/tiptap/node-views/file/Component.svelte
+++ packages/ui/src/tiptap/node-views/file/Component.svelte
@@ -1,8 +1,12 @@
 <script lang="ts">
   import { css } from '@readable/styled-system/css';
+  import { flex } from '@readable/styled-system/patterns';
   import { createFloatingActions } from '@readable/ui/actions';
   import { NodeView } from '@readable/ui/tiptap';
-  import Button from '../../../components/Button.svelte';
+  import FileUpIcon from '~icons/lucide/file-up';
+  import FolderIcon from '~icons/lucide/folder';
+  import Trash2Icon from '~icons/lucide/trash-2';
+  import { Button, Icon, RingSpinner } from '../../../components';
   import type { NodeViewProps } from '@readable/ui/tiptap';
 
   type $$Props = NodeViewProps;
@@ -12,6 +16,7 @@
   export let extension: NodeViewProps['extension'];
   export let selected: NodeViewProps['selected'];
   export let updateAttributes: NodeViewProps['updateAttributes'];
+  export let deleteNode: NodeViewProps['deleteNode'];
 
   let inflight = false;
   let pickerOpened = false;
@@ -19,7 +24,7 @@
 
   const { anchor, floating } = createFloatingActions({
     placement: 'bottom-start',
-    offset: 8,
+    offset: 12,
     onClickOutside: () => {
       pickerOpened = false;
     },
@@ -50,22 +55,90 @@
   };
 </script>
 
-<NodeView>
+<NodeView
+  style={css.raw(
+    {
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'space-between',
+      borderRadius: '10px',
+      backgroundColor: {
+        base: 'neutral.10',
+        _hover: 'neutral.20',
+      },
+      _hover: { '& > button': { display: 'flex' } },
+    },
+    pickerOpened && { backgroundColor: 'neutral.30' },
+  )}
+>
   {#if node.attrs.id}
-    <div class={css({ backgroundColor: 'neutral.20', padding: '12px' })}>{node.attrs.name}</div>
+    <div
+      class={flex({
+        align: 'center',
+        gap: '8px',
+        paddingX: '10px',
+        paddingY: '12px',
+        textStyle: '16m',
+      })}
+    >
+      <Icon icon={FileUpIcon} size={24} />
+      <span>{node.attrs.name}</span>
+    </div>
   {:else}
-    <div class={css({ backgroundColor: 'neutral.20', padding: '12px' })} use:anchor>
+    <div
+      class={flex({
+        align: 'center',
+        gap: '7px',
+        padding: '17px',
+        textStyle: '16m',
+        color: 'text.secondary',
+        width: 'full',
+      })}
+      use:anchor
+    >
       {#if inflight}
-        업로드 중...
+        <RingSpinner style={css.raw({ size: '28px', color: 'neutral.60' })} />
+        파일 업로드 중...
       {:else}
-        파일
+        <Icon icon={FolderIcon} size={16} />
+        파일 업로드
       {/if}
     </div>
   {/if}
+
+  <button
+    class={css({
+      display: 'none',
+      alignItems: 'center',
+      justifyContent: 'center',
+      marginRight: node.attrs.id ? '10px' : '17px',
+      borderRadius: '4px',
+      color: 'neutral.50',
+      size: '24px',
+      _hover: { backgroundColor: 'neutral.30' },
+    })}
+    type="button"
+    on:click={() => deleteNode()}
+  >
+    <Icon icon={Trash2Icon} size={12} />
+  </button>
 </NodeView>
 
 {#if pickerOpened && !node.attrs.id && !inflight}
-  <div class={css({ backgroundColor: 'neutral.10', paddingX: '36px', paddingY: '12px' })} use:floating>
-    <Button on:click={handleUpload}>업로드</Button>
+  <div
+    class={flex({
+      direction: 'column',
+      align: 'center',
+      gap: '14px',
+      borderRadius: '10px',
+      padding: '20px',
+      backgroundColor: 'surface.primary',
+      width: '460px',
+      boxShadow: 'heavy',
+    })}
+    use:floating
+  >
+    <Button style={css.raw({ width: 'full' })} size="lg" on:click={handleUpload}>파일 업로드</Button>
+    <span class={css({ textStyle: '13m', color: 'text.tertiary' })}>파일당 최대 크기는 5MB입니다</span>
   </div>
 {/if}
--- packages/ui/src/tiptap/node-views/image/Component.svelte
+++ packages/ui/src/tiptap/node-views/image/Component.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
   import { css } from '@readable/styled-system/css';
+  import { flex } from '@readable/styled-system/patterns';
   import { createFloatingActions } from '@readable/ui/actions';
   import { NodeView } from '@readable/ui/tiptap';
-  import Button from '../../../components/Button.svelte';
-  import Img from '../../../components/Img.svelte';
+  import ImageIcon from '~icons/lucide/image';
+  import Trash2Icon from '~icons/lucide/trash-2';
+  import { Button, Icon, Img, RingSpinner } from '../../../components';
   import type { NodeViewProps } from '@readable/ui/tiptap';
 
   type $$Props = NodeViewProps;
@@ -13,6 +15,7 @@
   export let extension: NodeViewProps['extension'];
   export let selected: NodeViewProps['selected'];
   export let updateAttributes: NodeViewProps['updateAttributes'];
+  export let deleteNode: NodeViewProps['deleteNode'];
 
   let inflight = false;
   let pickerOpened = false;
@@ -20,7 +23,7 @@
 
   const { anchor, floating } = createFloatingActions({
     placement: 'bottom-start',
-    offset: 8,
+    offset: 12,
     onClickOutside: () => {
       pickerOpened = false;
     },
@@ -54,28 +57,95 @@
 
 <NodeView>
   {#if node.attrs.id}
-    <Img
-      style={css.raw({ width: 'screen' })}
-      alt=""
-      placeholder={node.attrs.placeholder}
-      progressive
-      ratio={node.attrs.ratio}
-      size="full"
-      url={node.attrs.url}
-    />
+    <div class={css({ position: 'relative', _hover: { '& > button': { display: 'flex' } } })}>
+      <Img
+        style={css.raw({ width: 'screen' })}
+        alt=""
+        placeholder={node.attrs.placeholder}
+        progressive
+        ratio={node.attrs.ratio}
+        size="full"
+        url={node.attrs.url}
+      />
+
+      <button
+        class={css({
+          position: 'absolute',
+          top: '8px',
+          right: '8px',
+          display: 'none',
+          alignItems: 'center',
+          justifyContent: 'center',
+          borderRadius: '4px',
+          color: 'neutral.50',
+          backgroundColor: 'neutral.10',
+          size: '24px',
+          _hover: { backgroundColor: 'neutral.20' },
+        })}
+        type="button"
+        on:click={() => deleteNode()}
+      >
+        <Icon icon={Trash2Icon} size={12} />
+      </button>
+    </div>
   {:else}
-    <div class={css({ backgroundColor: 'neutral.20', padding: '12px' })} use:anchor>
-      {#if inflight}
-        업로드 중...
-      {:else}
-        이미지
-      {/if}
+    <div
+      class={css(
+        {
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'space-between',
+          gap: '7px',
+          borderRadius: '10px',
+          padding: '17px',
+          backgroundColor: { base: 'neutral.10', _hover: 'neutral.20' },
+        },
+        pickerOpened && { backgroundColor: 'neutral.30' },
+      )}
+      use:anchor
+    >
+      <div class={flex({ align: 'center', gap: '7px' })}>
+        {#if inflight}
+          <RingSpinner style={css.raw({ color: 'neutral.60', size: '28px' })} />
+        {:else}
+          <Icon style={css.raw({ color: 'neutral.60' })} icon={ImageIcon} size={18} />
+        {/if}
+        <span class={css({ textStyle: '16m', color: 'text.secondary' })}>이미지 업로드</span>
+      </div>
+
+      <button
+        class={flex({
+          align: 'center',
+          justify: 'center',
+          borderRadius: '4px',
+          color: 'neutral.50',
+          size: '24px',
+          _hover: { backgroundColor: 'neutral.30' },
+        })}
+        type="button"
+        on:click={() => deleteNode()}
+      >
+        <Icon icon={Trash2Icon} size={12} />
+      </button>
     </div>
   {/if}
 </NodeView>
 
 {#if pickerOpened && !node.attrs.id && !inflight}
-  <div class={css({ backgroundColor: 'neutral.10', paddingX: '36px', paddingY: '12px' })} use:floating>
-    <Button on:click={handleUpload}>업로드</Button>
+  <div
+    class={flex({
+      direction: 'column',
+      align: 'center',
+      gap: '14px',
+      borderRadius: '10px',
+      padding: '20px',
+      backgroundColor: 'surface.primary',
+      width: '460px',
+      boxShadow: 'heavy',
+    })}
+    use:floating
+  >
+    <Button style={css.raw({ width: 'full' })} size="lg" on:click={handleUpload}>이미지 업로드</Button>
+    <span class={css({ textStyle: '13m', color: 'text.tertiary' })}>파일당 최대 크기는 5MB입니다</span>
   </div>
 {/if}
