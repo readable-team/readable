3f6dd03c75dc354839c94c1fc9bb9f7449e8b0e7
dolsup <8360934+dolsup@users.noreply.github.com>
2024-08-19 03:30:31 +0000

    대시보드 가로 최소 1280px (#414)

--- apps/dashboard/src/routes/+layout.svelte
+++ apps/dashboard/src/routes/+layout.svelte
@@ -1,9 +1,16 @@
 <script lang="ts">
   import '../app.css';
 
+  import { css } from '@readable/styled-system/css';
   import { ToastProvider } from '@readable/ui/notification';
 </script>
 
-<slot />
+<div
+  class={css({
+    minWidth: '1280px',
+  })}
+>
+  <slot />
+</div>
 
 <ToastProvider />
