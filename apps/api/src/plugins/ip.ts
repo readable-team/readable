1cc741674f6321195b9be412cce89294151fffdf
devunt <337728+devunt@users.noreply.github.com>
2024-08-20 16:42:30 +0000

    클라이언트 ip 주소 판단하는 로직 버그 수정 (#441)

--- apps/api/src/plugins/ip.ts
+++ apps/api/src/plugins/ip.ts
@@ -1,12 +1,28 @@
 import Elysia from 'elysia';
 import IPAddr from 'ipaddr.js';
+import * as R from 'remeda';
 
 export const ip = new Elysia({ name: 'ip' }).derive({ as: 'scoped' }, ({ request, server }) => {
   const xff = request.headers.get('x-forwarded-for');
-  const ip =
-    xff?.split(',').findLast((ip) => IPAddr.isValid(ip) && IPAddr.process(ip).range() === 'private') ??
-    server?.requestIP(request)?.address ??
-    '0.0.0.0';
+  if (xff) {
+    const ip = R.pipe(
+      xff,
+      R.split(','),
+      R.map((v) => v.trim()),
+      R.filter((v) => IPAddr.isValid(v)),
+      R.map((v) => IPAddr.process(v)),
+      R.findLast((v) => v.range() !== 'private'),
+    );
 
-  return { ip };
+    if (ip) {
+      return { ip: ip.toString() };
+    }
+  }
+
+  const ip = server?.requestIP(request)?.address;
+  if (ip) {
+    return { ip: IPAddr.process(ip).toString() };
+  }
+
+  return { ip: '0.0.0.0' };
 });
