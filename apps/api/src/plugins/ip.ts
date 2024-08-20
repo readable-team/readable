8083cd95f101659243cbe796001c74cfcef40c05
devunt <337728+devunt@users.noreply.github.com>
2024-08-20 15:58:57 +0000

    ipv4-mapped ipv6 핸들링함 (#440)

--- apps/api/src/plugins/ip.ts
+++ apps/api/src/plugins/ip.ts
@@ -4,9 +4,9 @@ import IPAddr from 'ipaddr.js';
 export const ip = new Elysia({ name: 'ip' }).derive({ as: 'scoped' }, ({ request, server }) => {
   const xff = request.headers.get('x-forwarded-for');
   const ip =
-    xff?.split(',').findLast((ip) => IPAddr.isValid(ip) && IPAddr.parse(ip).range() === 'private') ??
+    xff?.split(',').findLast((ip) => IPAddr.isValid(ip) && IPAddr.process(ip).range() === 'private') ??
     server?.requestIP(request)?.address ??
-    '(unknown)';
+    '0.0.0.0';
 
   return { ip };
 });
