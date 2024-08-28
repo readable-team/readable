06bc97611886bf0e09bcc08ae6e170cec9532bcf
devunt <337728+devunt@users.noreply.github.com>
2024-08-28 09:44:11 +0000

    데이터베이스 시딩용 seed.sql 파일 추가 (#541)
    추후 재현 가능성을 위해...

---
+++ apps/api/scripts/seed.sql
@@ -0,0 +1,37 @@
+CREATE DATABASE readable;
+CREATE EXTENSION IF NOT EXISTS pg_stat_statements;
+
+CREATE ROLE readable WITH LOGIN PASSWORD '';
+CREATE ROLE datadog WITH LOGIN PASSWORD '';
+
+GRANT pg_monitor TO datadog;
+
+-- \c readable;
+
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO readable;
+
+CREATE SCHEMA datadog;
+GRANT USAGE ON SCHEMA datadog TO datadog;
+GRANT USAGE ON SCHEMA public TO datadog;
+
+CREATE OR REPLACE FUNCTION datadog.explain_statement(
+   l_query TEXT,
+   OUT explain JSON
+)
+RETURNS SETOF JSON AS
+$$
+DECLARE
+curs REFCURSOR;
+plan JSON;
+
+BEGIN
+   OPEN curs FOR EXECUTE pg_catalog.concat('EXPLAIN (FORMAT JSON) ', l_query);
+   FETCH curs INTO plan;
+   CLOSE curs;
+   RETURN QUERY SELECT plan;
+END;
+$$
+-- spell-checker:disable-next-line
+LANGUAGE 'plpgsql'
+RETURNS NULL ON NULL INPUT
+SECURITY DEFINER;
