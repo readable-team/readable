59afd34d4f69d4926c6546d7ef003da7de9aac98
devunt <337728+devunt@users.noreply.github.com>
2024-08-28 09:44:12 +0000

    프로덕션 RDS 생성 (#542)

--- apps/infrastructure/pulumi/aws/rds.ts
+++ apps/infrastructure/pulumi/aws/rds.ts
@@ -3,6 +3,19 @@ import * as random from '@pulumi/random';
 import { zones } from '$aws/route53';
 import { securityGroups, subnets } from '$aws/vpc';
 
+const monitoring = new aws.iam.Role('monitoring@rds', {
+  name: 'monitoring@rds',
+  assumeRolePolicy: aws.iam.assumeRolePolicyForPrincipal({
+    Service: 'monitoring.rds.amazonaws.com',
+  }),
+  managedPolicyArns: [aws.iam.ManagedPolicy.AmazonRDSEnhancedMonitoringRole],
+});
+
+const password = new random.RandomPassword('readable@rds', {
+  length: 20,
+  special: false,
+});
+
 const devPassword = new random.RandomPassword('readable-dev@rds', {
   length: 20,
   special: false,
@@ -19,6 +32,54 @@ const parameterGroup = new aws.rds.ClusterParameterGroup('readable', {
   family: 'aurora-postgresql16',
 
   parameters: [],
+});
+
+const cluster = new aws.rds.Cluster('readable', {
+  clusterIdentifier: 'readable',
+
+  engine: 'aurora-postgresql',
+  engineMode: 'provisioned',
+  engineVersion: '16.3',
+
+  dbClusterParameterGroupName: parameterGroup.name,
+
+  dbSubnetGroupName: subnetGroup.name,
+  vpcSecurityGroupIds: [securityGroups.internal.id],
+
+  deletionProtection: true,
+  storageEncrypted: true,
+
+  backupRetentionPeriod: 7,
+  finalSnapshotIdentifier: 'readable-final-snapshot',
+
+  preferredBackupWindow: '19:00-20:00',
+  preferredMaintenanceWindow: 'sun:20:00-sun:22:00',
+
+  masterUsername: 'root',
+  masterPassword: password.result,
+
+  applyImmediately: true,
+});
+
+const instance = new aws.rds.ClusterInstance('readable-1', {
+  clusterIdentifier: cluster.id,
+  identifier: 'readable-1',
+
+  engine: 'aurora-postgresql',
+  instanceClass: 'db.t4g.medium',
+
+  availabilityZone: subnets.private.az1.availabilityZone,
+  caCertIdentifier: 'rds-ca-ecc384-g1',
+
+  preferredMaintenanceWindow: 'sun:20:00-sun:22:00',
+
+  monitoringRoleArn: monitoring.arn,
+  monitoringInterval: 60,
+  performanceInsightsEnabled: true,
+
+  promotionTier: 0,
+
+  applyImmediately: true,
 });
 
 const devCluster = new aws.rds.Cluster('readable-dev', {
@@ -68,6 +129,14 @@ new aws.rds.ClusterInstance('readable-dev-1', {
   applyImmediately: true,
 });
 
+new aws.route53.Record('db.rdbl.app', {
+  zoneId: zones.rdbl_app.zoneId,
+  type: 'CNAME',
+  name: 'db.rdbl.app',
+  records: [cluster.endpoint],
+  ttl: 300,
+});
+
 new aws.route53.Record('dev.db.rdbl.app', {
   zoneId: zones.rdbl_app.zoneId,
   type: 'CNAME',
@@ -76,6 +145,12 @@ new aws.route53.Record('dev.db.rdbl.app', {
   ttl: 300,
 });
 
+export const db = {
+  cluster,
+  instance,
+};
+
 export const outputs = {
+  AWS_RDS_PASSWORD: password.result,
   AWS_RDS_DEV_PASSWORD: devPassword.result,
 };
---
+++ apps/infrastructure/pulumi/k8s/monitoring/dbm.ts
@@ -0,0 +1,47 @@
+import * as k8s from '@pulumi/kubernetes';
+import * as pulumi from '@pulumi/pulumi';
+import * as random from '@pulumi/random';
+import { db } from '$aws/rds';
+import { namespace } from '$k8s/monitoring';
+
+const password = new random.RandomPassword('monitoring/dbm@k8s', {
+  length: 20,
+  special: false,
+});
+
+new k8s.core.v1.Service('dbm', {
+  metadata: {
+    name: 'dbm',
+    namespace: namespace.metadata.name,
+    labels: {
+      'tags.datadoghq.com/env': 'prod',
+      'tags.datadoghq.com/service': 'readable',
+    },
+    annotations: {
+      'ad.datadoghq.com/service.checks': pulumi.jsonStringify({
+        postgres: {
+          instances: [
+            {
+              dbm: true,
+              host: db.instance.endpoint,
+              port: 5432,
+              username: 'datadog',
+              password: password.result,
+              // spell-checker:disable
+              dbname: 'readable',
+              dbstrict: true,
+              // spell-checker:enable
+            },
+          ],
+        },
+      }),
+    },
+  },
+  spec: {
+    clusterIP: 'None',
+  },
+});
+
+export const outputs = {
+  K8S_MONITORING_DBM_PASSWORD: password.result,
+};
