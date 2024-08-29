c2697206ea93770dcd7b156bc2bd7cd5da4081f4
devunt <337728+devunt@users.noreply.github.com>
2024-08-29 02:16:01 +0000

    대시보드 메인 접속시 사이트로 리다이렉트 (#551)

--- apps/dashboard/src/routes/(default)/+page.svelte
+++ apps/dashboard/src/routes/(default)/+page.svelte
@@ -1,22 +1,10 @@
 <script lang="ts">
-  import { css } from '@readable/styled-system/css';
-  import { Alert, Button, Helmet, TextInput } from '@readable/ui/components';
-  import { goto } from '$app/navigation';
   import { graphql } from '$graphql';
-  import { Img } from '$lib/components';
 
-  let siteName = '';
-  let deleteTeamOpen = false;
-
-  $: query = graphql(`
+  $: graphql(`
     query TeamPage_Query($teamId: ID!) {
       team(teamId: $teamId) {
         id
-        name
-        avatar {
-          id
-          ...Img_image
-        }
 
         sites {
           id
@@ -25,72 +13,4 @@
       }
     }
   `);
-
-  const createSite = graphql(`
-    mutation TeamPage_CreateSite_Mutation($input: CreateSiteInput!) {
-      createSite(input: $input) {
-        id
-      }
-    }
-  `);
-
-  const deleteTeam = graphql(`
-    mutation TeamPage_DeleteTeam_Mutation($input: DeleteTeamInput!) {
-      deleteTeam(input: $input) {
-        id
-      }
-    }
-  `);
 </script>
-
-<Helmet title={$query.team.name} />
-
-팀 메인 페이지
-
-<br />
-팀 이름: {$query.team.name}
-<Img
-  style={css.raw({ borderWidth: '1px', borderColor: 'border.image', borderRadius: 'full', size: '38px' })}
-  $image={$query.team.avatar}
-  alt={`${$query.team.name}의 아바타`}
-  size={48}
-/>
-<br />
-<Button on:click={() => (deleteTeamOpen = true)}>팀 삭제</Button>
-<br />
-<br />
-
-사이트 목록:
-<br />
-
-{#each $query.team.sites as site (site.id)}
-  <a href={`/${site.id}`}>{site.name}</a>
-  <br />
-{/each}
-
-<br />
-<br />
-<TextInput name="email" placeholder="새 사이트 이름" bind:value={siteName} />
-<br />
-<Button
-  on:click={async () => {
-    const site = await createSite({ teamId: $query.team.id, name: siteName });
-    await goto(`/${site.id}`);
-  }}
->
-  새 사이트 만들기
-</Button>
-
-<Alert
-  onAction={async () => {
-    await deleteTeam({ teamId: $query.team.id });
-    await goto('/', { invalidateAll: true });
-  }}
-  bind:open={deleteTeamOpen}
->
-  <svelte:fragment slot="title">팀을 삭제할까요?</svelte:fragment>
-  <svelte:fragment slot="content">팀을 삭제하면 팀에 속한 모든 사이트가 삭제돼요</svelte:fragment>
-
-  <svelte:fragment slot="action">삭제하기</svelte:fragment>
-  <svelte:fragment slot="cancel">삭제하지 않을래요</svelte:fragment>
-</Alert>
--- apps/dashboard/src/routes/(default)/+page.ts
+++ apps/dashboard/src/routes/(default)/+page.ts
@@ -1,6 +1,7 @@
+import { redirect } from '@sveltejs/kit';
 import { get } from 'svelte/store';
 import { currentTeamId } from '$lib/stores';
-import type { TeamPage_Query_Variables } from './$graphql';
+import type { TeamPage_Query_AfterLoad, TeamPage_Query_Variables } from './$graphql';
 
 export const _TeamPage_Query_Variables: TeamPage_Query_Variables = async ({ parent }) => {
   await parent();
@@ -9,4 +10,8 @@ export const _TeamPage_Query_Variables: TeamPage_Query_Variables = async ({ pare
     // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
     teamId: get(currentTeamId)!,
   };
+};
+
+export const _TeamPage_Query_AfterLoad: TeamPage_Query_AfterLoad = async (query) => {
+  redirect(302, `/${query.team.sites[0].id}`);
 };
