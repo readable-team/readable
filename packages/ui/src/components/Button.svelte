21965393563c950a01a8860e2b5b88c143a9a792
saeyanso <179500652+saeyanso@users.noreply.github.com>
2024-09-05 10:19:22 +0000

    danger-fill button 스타일링 수정 (#622)

--- packages/ui/src/components/Button.svelte
+++ packages/ui/src/components/Button.svelte
@@ -104,27 +104,22 @@
         'danger-fill': {
           color: {
             _enabled: {
-              base: { base: 'red.700', _dark: 'red.600' },
-              _hover: { base: 'red.700', _dark: 'red.600' },
-              _focusVisible: { base: 'red.700', _dark: 'red.600' },
-              _active: { base: 'red.700', _dark: 'red.600' },
-              _pressed: { base: 'red.700', _dark: 'red.600' },
+              base: { base: 'white', _dark: 'white' },
+              _hover: { base: 'white', _dark: 'white' },
+              _focusVisible: { base: 'white', _dark: 'white' },
+              _active: { base: 'white', _dark: 'white' },
+              _pressed: { base: 'white', _dark: 'white' },
             },
             _disabled: { base: 'gray.500', _dark: 'darkgray.700' },
           },
           backgroundColor: {
             _enabled: {
-              base: { base: 'red.100', _dark: 'red.800/20' },
-              _hover: { base: 'red.200', _dark: 'red.800/40' },
-              _focusVisible: { base: 'red.200', _dark: 'red.800/40' },
-              _active: { base: 'red.300', _dark: 'red.800/60' },
-              _pressed: { base: 'red.300', _dark: 'red.800/60' },
+              base: { base: 'red.600', _dark: 'red.500' },
+              _hover: { base: 'red.500', _dark: 'red.400' },
+              _focusVisible: { base: 'red.500', _dark: 'red.400' },
+              _active: { base: 'red.700', _dark: 'red.600' },
+              _pressed: { base: 'red.700', _dark: 'red.600' },
             },
-            _disabled: { base: 'gray.200', _dark: 'darkgray.900' },
-          },
-          borderWidth: '1px',
-          borderColor: {
-            _enabled: { base: 'red.200', _dark: 'red.800' },
             _disabled: { base: 'gray.200', _dark: 'darkgray.900' },
           },
         },
