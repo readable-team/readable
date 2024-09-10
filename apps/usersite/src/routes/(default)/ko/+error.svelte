a7edc037ad447ff9e3db17fdb546c3f61cb13458
Seohyun-Roh <76952602+Seohyun-Roh@users.noreply.github.com>
2024-09-10 03:05:26 +0000

    유저사이트 에러페이지 내용 중앙정렬되도록 수정 (#736)

--- apps/usersite/src/routes/(default)/ko/+error.svelte
+++ apps/usersite/src/routes/(default)/ko/+error.svelte
@@ -12,9 +12,11 @@
     direction: 'column',
     alignItems: 'center',
     gap: '24px',
+    marginY: 'auto',
   })}
 >
   <Icon style={css.raw({ width: '60px', height: '60px' })} icon={FileSearchIcon} />
+
   <div class={flex({ direction: 'column', alignItems: 'center', gap: '2px' })}>
     <h1 class={css({ fontSize: '22px', fontWeight: 'bold' })}>존재하지 않는 페이지입니다</h1>
     <p class={css({ textAlign: 'center', fontSize: '15px', color: 'text.secondary' })}>
@@ -23,5 +25,7 @@
       입력한 주소를 다시 한 번 확인해주세요.
     </p>
   </div>
+
+  <!-- TODO: 페이지 하나도 없을 때는 버튼 없애기 -->
   <Button href="/" size="lg" type="link" variant="secondary">홈으로 이동</Button>
 </div>
