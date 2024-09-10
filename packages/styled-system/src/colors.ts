842f3dbbdd4b9f250b34408fbde6ce832fee7c43
saeyanso <179500652+saeyanso@users.noreply.github.com>
2024-09-10 22:49:48 +0000

    text-tertiary 웹 접근성 FAIL 수정 (#753)
    자려고 누웠는데 갑자기 이런 생각이 드는 겁니다
    "폰트가 너무...연하지 않나?" 그래서 텍스트의 웹 접근성 기준을 하나하나 적용해보았습니다.
    ![CleanShot 2024-09-11 at 01 04 05@2x](https://github.com/user-attachments/assets/ae959a73-f9bf-4733-96cc-04c5d918d782)
    저런.. 이대로 출시했으면 큰일날뻔했네요

--- packages/styled-system/src/colors.ts
+++ packages/styled-system/src/colors.ts
@@ -61,10 +61,10 @@ export const semanticColors = defineSemanticTokens.colors({
   text: {
     accent: { value: { base: '{colors.brand.600}', _dark: '{colors.brand.500}' } },
     primary: { value: { base: '{colors.gray.1000}', _dark: '{colors.darkgray.100}' } },
-    secondary: { value: { base: '{colors.gray.600}', _dark: '{colors.darkgray.400}' } },
-    tertiary: { value: { base: '{colors.gray.500}', _dark: '{colors.darkgray.500}' } },
+    secondary: { value: { base: '{colors.gray.700}', _dark: '{colors.darkgray.300}' } },
+    tertiary: { value: { base: '{colors.gray.600}', _dark: '{colors.darkgray.400}' } },
     danger: { value: { base: '{colors.red.600}', _dark: '{colors.red.500}' } },
-    disabled: { value: { base: '{colors.gray.400}', _dark: '{colors.darkgray.600}' } },
+    disabled: { value: { base: '{colors.gray.500}', _dark: '{colors.darkgray.600}' } },
   },
   divider: {
     primary: { value: { base: '{colors.gray.200}', _dark: '{colors.darkgray.900}' } },
