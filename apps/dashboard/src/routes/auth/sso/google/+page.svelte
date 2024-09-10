ed0e1135b9ed1497a05436a7e04c0c6b2f377fea
dolsup <8360934+dolsup@users.noreply.github.com>
2024-09-10 06:41:52 +0000

    로그인 중 페이지 (#740)

--- apps/dashboard/src/routes/auth/sso/google/+page.svelte
+++ apps/dashboard/src/routes/auth/sso/google/+page.svelte
@@ -1,5 +1,8 @@
 <script lang="ts">
+  import { flex } from '@readable/styled-system/patterns';
   import { onMount } from 'svelte';
+  import { linear } from 'svelte/easing';
+  import { tweened } from 'svelte/motion';
   import { SingleSignOnProvider } from '@/enums';
   import { goto } from '$app/navigation';
   import { page } from '$app/stores';
@@ -24,6 +27,59 @@
 
     await goto('/');
   });
+
+  /**
+   * Logo animation
+   */
+  let visible = true;
+  let duration = 250;
+  const opacity = tweened(1, {
+    duration,
+    easing: linear,
+  });
+
+  onMount(() => {
+    const interval = setInterval(() => {
+      opacity.set(visible ? 0.75 : 1);
+      visible = !visible;
+    }, duration);
+
+    return () => clearInterval(interval);
+  });
 </script>
 
-logging in...
+<div
+  class={flex({
+    width: 'screen',
+    height: 'screen',
+    alignItems: 'center',
+    justifyContent: 'center',
+    background:
+      '[radial-gradient(135.27% 56.6% at 43.4% 46.41%, rgba(254, 219, 198, 0.00) 0%, rgba(255, 192, 156, 0.08) 100%), #FFF]',
+  })}
+>
+  <svg
+    style:opacity={$opacity}
+    fill="none"
+    height="81"
+    viewBox="0 0 80 81"
+    width="80"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <rect fill="url(#paint0_linear_1320_8674)" height="80" rx="12" width="80" y="0.5" />
+    <path d="M55.1963 16.9192V53.0066L26.2007 64.0808V27.9935L55.1963 16.9192Z" fill="white" />
+    <defs>
+      <linearGradient
+        id="paint0_linear_1320_8674"
+        gradientUnits="userSpaceOnUse"
+        x1="11.7031"
+        x2="68.821"
+        y1="0.499999"
+        y2="83.2948"
+      >
+        <stop stop-color="#FF7728" />
+        <stop offset="1" stop-color="#FF5E00" />
+      </linearGradient>
+    </defs>
+  </svg>
+</div>
