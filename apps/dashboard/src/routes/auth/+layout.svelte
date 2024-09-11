224ed22e6cb2519b088ea464aa161fc3ccf6afa0
Seohyun-Roh <76952602+Seohyun-Roh@users.noreply.github.com>
2024-09-11 07:05:43 +0000

    로그인 페이지 푸터 레이아웃 깨지는 문제 수정 (#781)

--- apps/dashboard/src/routes/auth/+layout.svelte
+++ apps/dashboard/src/routes/auth/+layout.svelte
@@ -5,7 +5,6 @@
 <div
   class={center({
     flexDirection: 'column',
-    width: 'screen',
     minHeight: 'screen',
     background:
       '[radial-gradient(135.27% 56.6% at 43.4% 46.41%, rgba(254, 219, 198, 0.00) 0%, rgba(255, 192, 156, 0.08) 100%), #FFF]',
--- apps/dashboard/src/routes/auth/login/+page.svelte
+++ apps/dashboard/src/routes/auth/login/+page.svelte
@@ -62,42 +62,43 @@
   </Button>
 </div>
 
-<footer
-  class={flex({
-    marginX: 'auto',
-    maxWidth: '1200px',
-    direction: 'column',
-    gap: '40px',
-    paddingX: { base: '24px', md: '48px' },
-    paddingY: '48px',
-    backgroundColor: 'neutral.0',
-    width: 'full',
-  })}
->
-  <div class={flex({ align: 'center', justify: 'space-between' })}>
-    <FullLogo class={css({ height: { base: '20px', md: '24px' } })} />
+<footer class={css({ backgroundColor: 'neutral.0', width: 'full' })}>
+  <div
+    class={flex({
+      marginX: 'auto',
+      maxWidth: '1200px',
+      direction: 'column',
+      gap: '40px',
+      paddingX: { base: '24px', md: '48px' },
+      paddingY: '48px',
+      width: 'full',
+    })}
+  >
+    <div class={flex({ align: 'center', justify: 'space-between' })}>
+      <FullLogo class={css({ height: { base: '20px', md: '24px' } })} />
 
-    <a aria-label="GitHub" href="https://github.com/readable-team" rel="noopener noreferrer" target="_blank">
-      <Icon style={css.raw({ color: '[#181717]', hideFrom: 'md' })} icon={IconGithub} size={20} />
-      <Icon style={css.raw({ color: '[#181717]', hideBelow: 'md' })} icon={IconGithub} size={24} />
-    </a>
-  </div>
-
-  <div class={flex({ textStyle: { base: '12r', md: '14r' }, color: 'neutral.70', direction: 'column', gap: '4px' })}>
-    <p>주식회사 펜슬컴퍼니 | 대표 배준현 | 서울특별시 강남구 강남대로100길 14, 6층</p>
-
-    <p>
-      사업자등록번호 610-88-03078 | <a
-        href="https://www.ftc.go.kr/bizCommPop.do?wrkr_no=6108803078"
-        rel="noopener noreferrer"
-        target="_blank"
-      >
-        통신판매업신고 2023-서울강남-4541
+      <a aria-label="GitHub" href="https://github.com/readable-team" rel="noopener noreferrer" target="_blank">
+        <Icon style={css.raw({ color: '[#181717]', hideFrom: 'md' })} icon={IconGithub} size={20} />
+        <Icon style={css.raw({ color: '[#181717]', hideBelow: 'md' })} icon={IconGithub} size={24} />
       </a>
-    </p>
+    </div>
 
-    <p>02-565-7695 | hello@penxle.io</p>
+    <div class={flex({ textStyle: { base: '12r', md: '14r' }, color: 'neutral.70', direction: 'column', gap: '4px' })}>
+      <p>주식회사 펜슬컴퍼니 | 대표 배준현 | 서울특별시 강남구 강남대로100길 14, 6층</p>
+
+      <p>
+        사업자등록번호 610-88-03078 | <a
+          href="https://www.ftc.go.kr/bizCommPop.do?wrkr_no=6108803078"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          통신판매업신고 2023-서울강남-4541
+        </a>
+      </p>
+
+      <p>02-565-7695 | hello@penxle.io</p>
+    </div>
+
+    <div class={css({ height: '20px' })}></div>
   </div>
-
-  <div class={css({ height: '20px' })}></div>
 </footer>
