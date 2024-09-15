c414e6d34e2262f76d2816d3bc8bd16daa3cda06
devunt <337728+devunt@users.noreply.github.com>
2024-09-15 03:53:16 +0000

    mailchimp 인증 레코드 추가 (#878)

--- apps/infrastructure/pulumi/aws/route53.ts
+++ apps/infrastructure/pulumi/aws/route53.ts
@@ -1,3 +1,5 @@
+// spell-checker:ignore mcsv
+
 import * as aws from '@pulumi/aws';
 
 const createZone = (domain: string) => {
@@ -20,5 +22,29 @@ new aws.route53.Record('rdbl.io|txt', {
     // spell-checker:disable-next-line
     'google-site-verification=aVWE05YPOxEGxeH-X5NRyfb_IYilB3KL7QlN-xkFrU0',
   ],
+  ttl: 300,
+});
+
+new aws.route53.Record('rdbl.io|mx', {
+  zoneId: zones.rdbl_io.zoneId,
+  type: 'MX',
+  name: 'rdbl.io',
+  records: ['1 smtp.google.com'],
+  ttl: 300,
+});
+
+new aws.route53.Record('k2._domainkey.rdbl.io', {
+  zoneId: zones.rdbl_io.zoneId,
+  type: 'CNAME',
+  name: 'k2._domainkey.rdbl.io',
+  records: ['dkim2.mcsv.net'],
+  ttl: 300,
+});
+
+new aws.route53.Record('k3._domainkey.rdbl.io', {
+  zoneId: zones.rdbl_io.zoneId,
+  type: 'CNAME',
+  name: 'k3._domainkey.rdbl.io',
+  records: ['dkim3.mcsv.net'],
   ttl: 300,
 });
