08db8240d68530a1f247a737d0dd5e7690d20a62
Seohyun-Roh <76952602+Seohyun-Roh@users.noreply.github.com>
2024-09-25 12:09:03 +0000

    랜딩 페이지 캐러셀 하단 섹션 퍼블리싱 (#994)

---
+++ apps/website/src/routes/ClientsSection.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+  import { css } from '@readable/styled-system/css';
+  import { flex } from '@readable/styled-system/patterns';
+  import GlyphLogo from '$assets/logos/glyph.svg?component';
+  import PlanetLogo from '$assets/logos/planet.svg?component';
+  import ZerobasedLogo from '$assets/logos/zerobased.svg?component';
+</script>
+
+<div class={css({ paddingY: { base: '54px', lg: '120px' }, paddingX: '20px' })}>
+  <!-- prettier-ignore -->
+  <p class={css({ textStyle: { base: '13m', lg: '22m' }, textAlign: 'center', color: 'text.secondary' })}>
+    복잡한 설정 없이도
+    <b class={css({ textStyle: { base: '13b', lg: '22b' }, color: 'text.primary' })}>전문가 수준의 도움센터를 제작</b>하세요
+    <br />
+    다양한 규모의 팀이 리더블과 함께하고 있습니다
+  </p>
+
+  <div
+    class={flex({
+      direction: 'column',
+      align: 'center',
+      justify: 'center',
+      gap: { base: '8px', lg: '24px' },
+      marginTop: '34px',
+      lg: { flexDirection: 'row' },
+    })}
+  >
+    <div class={css({ paddingTop: '14px', paddingBottom: '9px', paddingX: '29px' })}>
+      <GlyphLogo class={css({ flex: 'none', height: '24px' })} />
+    </div>
+    <div class={css({ paddingY: '14px', paddingX: '17px' })}>
+      <ZerobasedLogo class={css({ flex: 'none', height: '20px' })} />
+    </div>
+    <div class={css({ paddingY: '11px', paddingX: '30px' })}>
+      <PlanetLogo class={css({ flex: 'none', height: '26px' })} />
+    </div>
+  </div>
+</div>
---
+++ apps/website/src/routes/CtaSection.svelte
@@ -0,0 +1,64 @@
+<script lang="ts">
+  import { css } from '@readable/styled-system/css';
+  import { flex } from '@readable/styled-system/patterns';
+  import { Button, Icon } from '@readable/ui/components';
+  import ChevronRightIcon from '~icons/lucide/chevron-right';
+  import { env } from '$env/dynamic/public';
+</script>
+
+<div
+  class={css({
+    paddingY: { base: '54px', lg: '120px' },
+    background: '[linear-gradient(108deg, rgba(255, 123, 46, 1.00) 4.37%, rgba(255, 106, 69, 1.00) 85.95%)]',
+  })}
+>
+  <div
+    class={flex({
+      align: 'center',
+      justify: 'space-between',
+      gap: '40px',
+      marginX: 'auto',
+      paddingX: '20px',
+      maxWidth: '1280px',
+      lgDown: { flexDirection: 'column' },
+    })}
+  >
+    <div class={css({ color: 'white' })}>
+      <h2
+        class={css({
+          marginBottom: { base: '10px', lg: '16px' },
+          fontSize: { base: '18px', lg: '[45px]' },
+          fontWeight: '[800]',
+          lgDown: { textAlign: 'center' },
+        })}
+      >
+        지금, 가이드문서가 없어도
+        <br />
+        시작해보세요
+      </h2>
+
+      <p class={css({ textStyle: { base: '13m', lg: '20m' }, lgDown: { textAlign: 'center' } })}>
+        기존 문서를 쉽게 임포트하고
+        <br />
+        즉시 리더블의 강력한 기능을 경험하세요
+      </p>
+    </div>
+
+    <div class={flex({ align: 'center', gap: { base: '16px', lg: '32px' }, lgDown: { flexDirection: 'column' } })}>
+      <Button href={env.PUBLIC_DASHBOARD_URL} size="lg" type="link" variant="secondary">지금 써보러가기</Button>
+
+      <a
+        class={flex({
+          gap: '6px',
+          alignItems: 'center',
+          textStyle: '18sb',
+          color: 'white',
+        })}
+        href="https://docs.rdbl.io"
+      >
+        활용 사례 보기
+        <Icon icon={ChevronRightIcon} size={20} />
+      </a>
+    </div>
+  </div>
+</div>
