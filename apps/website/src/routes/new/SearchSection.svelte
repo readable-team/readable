6a5eae412569e4370ac427b04d75a51f9a0c19da
Seohyun-Roh <76952602+Seohyun-Roh@users.noreply.github.com>
2024-09-26 06:44:38 +0000

    랜딩 검색바 dom으로 전환 (#1009)

--- apps/website/src/routes/new/SearchSection.svelte
+++ apps/website/src/routes/new/SearchSection.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
   import { css } from '@readable/styled-system/css';
   import { center, flex } from '@readable/styled-system/patterns';
-  import { Icon } from '@readable/ui/components';
+  import { HorizontalDivider, Icon } from '@readable/ui/components';
+  import CircleXIcon from '~icons/lucide/circle-x';
   import CodeXmlIcon from '~icons/lucide/code-xml';
-  import SearchDemo from './SearchDemo.svelte';
+  import SearchIcon from '~icons/lucide/search';
+  import SparklesIcon from '$assets/icons/sparkles.svg?component';
   import SectionTitle from './SectionTitle.svelte';
 
   const cards = [
@@ -52,7 +54,97 @@
         marginTop: '100px',
       })}
     >
-      <SearchDemo />
+      <div
+        class={flex({
+          flexDirection: 'column',
+          position: 'relative',
+          zIndex: '100',
+          marginX: 'auto',
+          borderRadius: { base: '16px', lg: '[22px]' },
+          width: 'full',
+          maxWidth: '867px',
+          padding: '8px',
+          backgroundColor: 'surface.primary',
+          boxShadow: 'strong',
+        })}
+      >
+        <div class={flex({ align: 'center', shrink: 0 })}>
+          <div
+            class={flex({
+              alignItems: 'center',
+              borderWidth: '2px',
+              borderColor: '[#9C3BE8]',
+              borderRadius: { base: '8px', lg: '14px' },
+              paddingX: { base: '12px', lg: '18px' },
+              textStyle: { base: '14m', lg: '16m' },
+              backgroundColor: { base: 'white', _dark: 'darkgray.1000' },
+              width: 'full',
+              height: { base: '36px', lg: '49px' },
+            })}
+          >
+            <span class={css({ padding: '2px', color: 'neutral.50' })}>
+              <Icon style={css.raw({ hideFrom: 'lg' })} icon={SearchIcon} size={16} />
+              <Icon style={css.raw({ hideBelow: 'lg' })} icon={SearchIcon} size={18} />
+            </span>
+
+            <p class={css({ marginLeft: '8px', width: 'full' })}>팀 이름을 변경하려면 어떻게 해야해?</p>
+
+            <div class={css({ marginLeft: '14px', color: 'neutral.50' })}>
+              <Icon style={css.raw({ hideFrom: 'lg' })} icon={CircleXIcon} size={16} />
+              <Icon style={css.raw({ hideBelow: 'lg' })} icon={CircleXIcon} size={18} />
+            </div>
+          </div>
+        </div>
+
+        <div
+          class={flex({
+            gap: '12px',
+            marginTop: '12px',
+            paddingX: '18px',
+            paddingY: '16px',
+          })}
+        >
+          <Icon style={css.raw({ color: '[#9C3BE8]' })} icon={SparklesIcon} size={20} />
+
+          <div>
+            <p class={css({ textStyle: '14r' })}>
+              팀 이름을 변경하려면 팀 대시보드의 팀 설정 탭으로 이동해야 합니다.
+              <br />
+              여기서 팀의 기본 정보를 변경할 수 있으며, 팀 로고와 함께 팀 이름도 수정할 수 있습니다. 이러한 변경 사항은 팀의
+              모든 멤버에게 적용되지만,
+              <br />
+              팀 로고와 팀 이름은 사이트에 노출되지 않습니다.
+            </p>
+
+            <HorizontalDivider style={css.raw({ marginTop: '12px', marginBottom: '8px' })} />
+
+            <div>
+              <div class={flex({ gap: '6px', alignItems: 'center' })}>
+                <div
+                  class={center({
+                    size: '17px',
+                    backgroundColor: 'neutral.30',
+                    color: 'text.tertiary',
+                    textStyle: '12sb',
+                    borderRadius: 'full',
+                  })}
+                >
+                  1
+                </div>
+                <span
+                  class={css({
+                    textStyle: { base: '12r', lg: '13r' },
+                    color: 'text.secondary',
+                    textDecoration: 'underline',
+                  })}
+                >
+                  팀과 사이트
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
 
     <div class={flex({ marginTop: '100px', gap: '15px' })}>
