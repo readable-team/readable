106109608edfcc2a8a5c2956fe003795907a7fc7
dolsup <8360934+dolsup@users.noreply.github.com>
2024-09-26 07:42:59 +0000

    FormProvider 디버그 (#1013)
    $$Props를 전부 집어넣으니까 짧은 시간동안 form action의 내용이 텍스트로 렌더링되는 버그가 있음

--- packages/ui/src/components/FormProvider.svelte
+++ packages/ui/src/components/FormProvider.svelte
@@ -1,12 +1,19 @@
 <script lang="ts">
   import type { FormAction } from '@felte/core';
+  import type { HTMLFormAttributes } from 'svelte/elements';
 
   export let form: FormAction;
   export let context: () => void;
 
+  type $$Props = {
+    form: FormAction;
+    context: () => void;
+  } & HTMLFormAttributes;
+
+  $: ({ form, context, ...rest } = $$props as $$Props);
   context();
 </script>
 
-<form use:form {...$$props}>
+<form use:form {...rest}>
   <slot />
 </form>
