4bf2468a10c4233975faaef6d278187bb26b2006
devunt <337728+devunt@users.noreply.github.com>
2024-09-26 03:37:44 +0000

    라이트하우스 애니메이션 추가 (#1001)

--- apps/website/src/routes/new/Lighthouse.svelte
+++ apps/website/src/routes/new/Lighthouse.svelte
@@ -1,18 +1,67 @@
 <script lang="ts">
   import { css } from '@readable/styled-system/css';
   import { center, flex } from '@readable/styled-system/patterns';
+  import { onMount } from 'svelte';
+  import { quintOut } from 'svelte/easing';
+  import { tweened } from 'svelte/motion';
+  import { derived } from 'svelte/store';
 
   export let title: string;
+
+  let containerEl: HTMLDivElement;
+
+  const circumference = 2 * Math.PI * 45;
+
+  const progress = tweened(0, { duration: 2000, easing: quintOut });
+  const value = derived(progress, ($progress) => Math.round($progress * 100));
+  const color = derived(progress, ($progress) =>
+    $progress > 0.9 ? '#00CC66' : $progress > 0.5 ? '#FFAA33' : '#FF3333',
+  );
+  const secondaryColor = derived(progress, ($progress) =>
+    $progress > 0.9 ? '#008800' : $progress > 0.5 ? '#C33300' : '#CC0000',
+  );
+
+  onMount(() => {
+    if (containerEl) {
+      const observer = new IntersectionObserver((entries) => {
+        if (entries.some((entry) => entry.isIntersecting)) {
+          observer.disconnect();
+          progress.set(1);
+        }
+      });
+
+      observer.observe(containerEl);
+
+      return () => observer.disconnect();
+    }
+  });
 </script>
 
 <!-- TODO: animation -->
-<div class={flex({ flexDirection: 'column', alignItems: 'center', gap: '14px' })}>
+<div bind:this={containerEl} class={flex({ flexDirection: 'column', alignItems: 'center', gap: '14px' })}>
   <div class={center({ position: 'relative', width: '201px', height: '201px' })}>
-    <svg fill="none" height="201" viewBox="0 0 201 201" width="201" xmlns="http://www.w3.org/2000/svg">
-      <circle cx="100.5" cy="100.5" fill="#EFFBF4" r="92.5" stroke="#0AD266" stroke-width="16" />
+    <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+      <circle
+        class={css({
+          transitionProperty: 'stroke, fill',
+          transitionDuration: '250ms',
+          transitionTimingFunction: 'ease',
+        })}
+        cx="50"
+        cy="50"
+        fill={`${$color}1A`}
+        r="45"
+        stroke={$color}
+        stroke-dasharray={circumference}
+        stroke-dashoffset={circumference * (1 - $progress)}
+        stroke-linecap="round"
+        stroke-width="10"
+        transform="rotate(-90 50 50)"
+      />
     </svg>
 
     <div
+      style:color={$secondaryColor}
       class={css({
         position: 'absolute',
         top: '[50%]',
@@ -20,11 +69,13 @@
         transform: 'translate(-50%, -50%)',
         fontFamily: 'mono',
         fontWeight: '[600]',
-        color: '[#008805]',
         fontSize: '[64px]',
+        transitionProperty: 'color',
+        transitionDuration: '250ms',
+        transitionTimingFunction: 'ease',
       })}
     >
-      100
+      {$value}
     </div>
   </div>
 
