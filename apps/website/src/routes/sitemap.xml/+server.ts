961f5a0de52491e8238c6bdc5065498df5db7802
devunt <337728+devunt@users.noreply.github.com>
2024-09-27 12:26:10 +0000

    웹사이트 사이트맵 갱신 (#1074)

--- apps/website/src/routes/sitemap.xml/+server.ts
+++ apps/website/src/routes/sitemap.xml/+server.ts
@@ -1,5 +1,5 @@
 import { sitemap } from '@readable/lib/svelte';
 
 export const GET = async (event) => {
-  return sitemap(event, ['/']);
+  return sitemap(event, ['/', '/pricing', '/preview', '/contact']);
 };
