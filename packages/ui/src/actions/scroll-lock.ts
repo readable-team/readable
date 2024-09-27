1b21d976602c2ffe24c5ffc222001fe6eb12697f
dolsup <8360934+dolsup@users.noreply.github.com>
2024-09-27 08:17:08 +0000

    body-scroll-lock 대신 body-scroll-lock-upgrade 사용 (#1042)
    https://github.com/willmcpo/body-scroll-lock/issues/182

--- packages/ui/src/actions/scroll-lock.ts
+++ packages/ui/src/actions/scroll-lock.ts
@@ -1,13 +1,17 @@
-import { disableBodyScroll, enableBodyScroll } from 'body-scroll-lock';
+import { disableBodyScroll, enableBodyScroll } from 'body-scroll-lock-upgrade';
 import type { Action } from 'svelte/action';
 
 export const scrollLock: Action<HTMLElement> = (element) => {
   disableBodyScroll(element, {
     allowTouchMove: (el) => {
       while (el !== document.body) {
-        if (el instanceof HTMLElement && el.dataset.scrollLockIgnore !== undefined) return true;
-        if (el.parentElement === null) return false;
-        el = el.parentElement;
+        if (el instanceof HTMLElement) {
+          if (el.dataset.scrollLockIgnore !== undefined) return true;
+          if (el.parentElement === null) return false;
+          el = el.parentElement;
+        } else {
+          return false;
+        }
       }
 
       return false;
