95308c5c924805d5292d1e966a9ce0b9daa0df3c
robin-maki <8201350+robin-maki@users.noreply.github.com>
2024-09-30 08:03:06 +0000

    피드백 팀 id gql (#1119)

--- apps/api/src/graphql/feedback.ts
+++ apps/api/src/graphql/feedback.ts
@@ -1,4 +1,6 @@
+import { and, eq } from 'drizzle-orm';
 import { builder } from '@/builder';
+import { db, firstOrThrow, TeamMembers, Teams, Users } from '@/db';
 import { logToSlack } from '@/utils/slack';
 
 builder.mutationFields((t) => ({
@@ -6,10 +8,26 @@ builder.mutationFields((t) => ({
     type: 'Boolean',
     input: {
       content: t.input.string(),
+      teamId: t.input.string(),
     },
-    resolve: async (_, { input }) => {
+    resolve: async (_, { input }, ctx) => {
+      const user = await db
+        .select({
+          id: Users.id,
+          name: Users.name,
+          teamId: Teams.id,
+          teamName: Teams.name,
+        })
+        .from(Users)
+        .innerJoin(TeamMembers, and(eq(TeamMembers.userId, Users.id), eq(TeamMembers.teamId, input.teamId)))
+        .innerJoin(Teams, eq(Teams.id, TeamMembers.teamId))
+        .where(eq(Users.id, ctx.session.userId))
+        .then(firstOrThrow);
+
       logToSlack('feedback', {
-        content: input.content,
+        content: `사용자: ${user.name} (${user.id})
+팀: ${user.teamName} (${user.teamId})
+내용: ${input.content}`,
       });
 
       return true;
