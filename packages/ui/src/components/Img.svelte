9faf9012804e53f9ddd5e42769165704d158ac73
Jiwon Choi <1890mah@gmail.com>
2024-10-07 16:19:32 +0900

    투명 PNG 이미지 뒤에 placeholder가 보이는 문제 디버그

--- packages/ui/src/components/Img.svelte
+++ packages/ui/src/components/Img.svelte
@@ -88,10 +88,10 @@
       overflow: 'hidden',
     })}
   >
-    <img style:aspect-ratio={ratio} class={css(style)} {alt} loading="lazy" src={placeholderUrl} />
     {#if loaded}
-      <div bind:this={contentEl} class={css({ position: 'absolute', inset: '0' })} in:fade={{ duration: 200 }} />
+      <div bind:this={contentEl} style:aspect-ratio={ratio} in:fade={{ duration: 200 }} />
     {:else}
+      <img style:aspect-ratio={ratio} class={css(style)} {alt} loading="lazy" src={placeholderUrl} />
       <div class={center({ position: 'absolute', inset: '0' })}>
         <RingSpinner style={css.raw({ size: '20px' })} />
       </div>
