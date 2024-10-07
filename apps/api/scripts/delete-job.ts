aea8edd390ed906ea0369691f1b891611520c5a1
robin-maki <8201350+robin-maki@users.noreply.github.com>
2024-10-07 05:29:55 +0000

    오래된 job 삭제 스크립트 (#1157)
    성공시 1주일 뒤 실패시 1달 뒤 삭제

---
+++ apps/api/scripts/delete-job.ts
@@ -0,0 +1,9 @@
+/* eslint-disable unicorn/no-process-exit */
+import dayjs from 'dayjs';
+import { and, eq, lt } from 'drizzle-orm';
+import { db, Jobs } from '@/db';
+import { JobState } from '@/enums';
+
+await db.delete(Jobs).where(and(eq(Jobs.state, JobState.COMPLETED), lt(Jobs.updatedAt, dayjs().subtract(1, 'week'))));
+await db.delete(Jobs).where(and(eq(Jobs.state, JobState.FAILED), lt(Jobs.updatedAt, dayjs().subtract(1, 'month'))));
+process.exit(0);
