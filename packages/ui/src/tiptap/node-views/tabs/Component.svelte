d1bf3a0d52527e37502ba44c990af58750d7d59a
Seohyun-Roh <76952602+Seohyun-Roh@users.noreply.github.com>
2024-10-10 09:46:17 +0000

    tabs 디자인 반영 및 구조 개선 (#1211)

--- packages/ui/src/tiptap/node-views/tabs/Component.svelte
+++ packages/ui/src/tiptap/node-views/tabs/Component.svelte
@@ -57,122 +57,131 @@
       align: 'center',
       position: 'relative',
       borderWidth: '1px',
+      borderBottomWidth: '0',
       borderColor: 'border.secondary',
-      borderTopRadius: '2px',
+      borderTopRadius: '4px',
       backgroundColor: 'gray.200',
-      height: '34px',
+      overflow: 'hidden',
     })}
     contenteditable="false"
   >
-    <div class={css({ overflowX: 'auto', scrollbar: 'hidden' })}>
-      <ul class={css({ display: 'flex' })} role="tablist">
-        {#each tabs as tab, i (i)}
-          <li
-            class={css(
-              {
-                'position': 'relative',
-                'borderWidth': '1px',
-                'borderBottomWidth': '0',
-                'borderColor': 'border.secondary',
-                'paddingLeft': '16px',
-                'paddingRight': editor?.isEditable ? '44px' : '16px',
-                'paddingY': '5px',
-                'flex': 'none',
-                'textStyle': '14m',
-                'height': '34px',
-                'cursor': 'pointer',
-                '_first': { borderLeftWidth: '0', borderTopLeftRadius: '2px' },
-                '_hover': { '& > button': { display: 'flex' } },
-                '& + &': { marginLeft: '-1px' },
-              },
-              tab.selected ? { backgroundColor: 'neutral.0' } : { color: 'text.secondary' },
-              !editor?.isEditable && { _last: { borderRightWidth: '0' } },
-            )}
-            role="tab"
-            tabindex={0}
-            on:keydown={null}
-            on:click={() => {
-              if (tab.selected) {
-                if (editor?.isEditable) {
-                  renamingTabIdx = i;
-                  renamingTabTitle = tab.title;
-                  tick().then(() => {
-                    renamingTabInput?.focus();
-                  });
+    <ul
+      class={css({
+        display: 'flex',
+        height: '34px',
+        overflowX: 'auto',
+        scrollbar: 'hidden',
+      })}
+      role="tablist"
+    >
+      {#each tabs as tab, i (i)}
+        <li
+          class={css(
+            {
+              position: 'relative',
+              borderRightWidth: '1px',
+              borderColor: 'border.secondary',
+              paddingLeft: '16px',
+              paddingRight: editor?.isEditable ? '44px' : '16px',
+              paddingY: '5px',
+              flex: 'none',
+              textStyle: '14m',
+              cursor: 'pointer',
+              _hover: { '& > button': { display: 'flex' } },
+            },
+            tab.selected ? { backgroundColor: 'neutral.0' } : { borderBottomWidth: '1px', color: 'text.secondary' },
+            !editor?.isEditable && { _last: { borderRightWidth: '0' } },
+          )}
+          role="tab"
+          tabindex={0}
+          on:keydown={null}
+          on:click={() => {
+            if (tab.selected) {
+              if (editor?.isEditable) {
+                renamingTabIdx = i;
+                renamingTabTitle = tab.title;
+                tick().then(() => {
+                  renamingTabInput?.focus();
+                });
+              }
+            } else {
+              editor
+                ?.chain()
+                .setTextSelection(getPos() + 1)
+                .selectTab(i)
+                .run();
+            }
+          }}
+        >
+          {#if renamingTabIdx === i}
+            <input
+              bind:this={renamingTabInput}
+              type="text"
+              bind:value={renamingTabTitle}
+              on:blur={handleRenameTab}
+              on:keydown={(e) => {
+                if (e.key === 'Enter') {
+                  handleRenameTab();
                 }
-              } else {
+              }}
+            />
+          {:else}
+            <span>
+              {tab.title}
+            </span>
+          {/if}
+
+          {#if editor?.isEditable}
+            <button
+              class={center({
+                position: 'absolute',
+                translate: 'auto',
+                translateY: '-1/2',
+                top: '1/2',
+                right: '14px',
+                display: tab.selected ? 'flex' : 'none',
+                size: '24px',
+                borderRadius: '4px',
+                color: 'gray.500',
+                _hover: { backgroundColor: 'gray.300/40' },
+              })}
+              type="button"
+              on:click|stopPropagation={() => {
                 editor
                   ?.chain()
                   .setTextSelection(getPos() + 1)
-                  .selectTab(i)
+                  .deleteTab(i)
                   .run();
-              }
-            }}
-          >
-            {#if renamingTabIdx === i}
-              <input
-                bind:this={renamingTabInput}
-                type="text"
-                bind:value={renamingTabTitle}
-                on:blur={handleRenameTab}
-                on:keydown={(e) => {
-                  if (e.key === 'Enter') {
-                    handleRenameTab();
-                  }
-                }}
-              />
-            {:else}
-              <span>
-                {tab.title}
-              </span>
-            {/if}
+              }}
+            >
+              <Icon icon={XIcon} />
+            </button>
+          {/if}
+        </li>
+      {/each}
+    </ul>
 
-            {#if editor?.isEditable}
-              <button
-                class={center({
-                  position: 'absolute',
-                  translate: 'auto',
-                  translateY: '-1/2',
-                  top: '1/2',
-                  right: '14px',
-                  display: tab.selected ? 'flex' : 'none',
-                  size: '24px',
-                  borderRadius: '4px',
-                  color: 'gray.500',
-                  _hover: { backgroundColor: 'gray.300/40' },
-                })}
-                type="button"
-                on:click|stopPropagation={() => {
-                  editor
-                    ?.chain()
-                    .setTextSelection(getPos() + 1)
-                    .deleteTab(i)
-                    .run();
-                }}
-              >
-                <Icon icon={XIcon} />
-              </button>
-            {/if}
-          </li>
-        {/each}
-      </ul>
-    </div>
-
-    {#if editor?.isEditable}
-      <div
-        class={center({
+    <div
+      class={css(
+        {
           position: 'sticky',
           top: '0',
           right: '0',
           zIndex: '1',
+          display: 'flex',
+          alignItems: 'center',
           flex: 'none',
-          borderYWidth: '1px',
+          flexGrow: '1',
+          borderBottomWidth: '1px',
           borderColor: 'border.secondary',
-          borderTopRightRadius: '2px',
-          size: '34px',
+          borderTopRightRadius: '4px',
+          height: '34px',
           backgroundColor: 'gray.200',
-        })}
-      >
+        },
+        editor?.isEditable && { paddingX: '5px', minWidth: '34px' },
+      )}
+    >
+      {#if editor?.isEditable}
         <button
           class={center({
             size: '24px',
@@ -191,8 +200,8 @@
         >
           <Icon icon={PlusIcon} size={16} />
         </button>
-      </div>
-    {/if}
+      {/if}
+    </div>
   </div>
 
   <div
@@ -200,7 +209,7 @@
       'borderWidth': '1px',
       'borderTopWidth': '0',
       'borderColor': 'border.secondary',
-      'borderBottomRadius': '2px',
+      'borderBottomRadius': '4px',
       'padding': '16px',
       '&:not(:has([data-tab-selected])) [role="tabpanel"]:first-child': { display: 'block' },
       '& [role="tabpanel"][data-tab-selected]': { display: 'block' },
