ef5c2fad9649de40476dc90fdc27b337f7e0de5a
devunt <337728+devunt@users.noreply.github.com>
2024-10-11 12:05:55 +0000

    노드뷰 안의 노드뷰가 올바르게 ssr 되도록 수정 (#1232)

--- packages/ui/src/tiptap/lib/create.ts
+++ packages/ui/src/tiptap/lib/create.ts
@@ -28,12 +28,11 @@ export const extendNodeToNodeView = <Options = any, Storage = any>(
     },
 
     renderHTML({ node, HTMLAttributes }) {
-      const attributes = mergeAttributes(HTMLAttributes, {
-        'data-node-view-type': options?.name ?? this.name,
-        'style': options?.inline ? 'display: inline-block;' : 'display: block;',
-      });
-
       if (browser) {
+        const attributes = mergeAttributes(HTMLAttributes, {
+          'data-node-view-type': options?.name ?? this.name,
+        });
+
         return node.isLeaf ? ['node-view', attributes] : ['node-view', attributes, 0];
       } else {
         // @ts-expect-error svelte internal
@@ -43,18 +42,9 @@ export const extendNodeToNodeView = <Options = any, Storage = any>(
           selected: false,
         });
 
-        // prosemirror-model이 ssr시에 zeed-dom을 이용하고, zeed-dom은 html attribute를 특별하게 처리하는 로직이 있어
-        // html attribute에 들어간 값을 innerHTML에 그대로 렌더링함. 아래 코드는 해당 동작에 의존함 (문서화되지 않은 기능 / 편법에 가까움)
-        // https://github.com/holtwick/zeed-dom/blob/6a2d6694ed879da9444d9b7c1826cef4b23c8a17/src/html.ts#L139-L140
-        if (node.isLeaf) {
-          return ['node-view', mergeAttributes(attributes, { html, style: 'white-space: normal;' })];
-        } else {
-          return [
-            'node-view',
-            mergeAttributes(attributes, { html, style: 'white-space: normal;' }),
-            ['node-view-content-editable', { style: 'display: block; white-space: normal;' }, 0],
-          ];
-        }
+        return node.isLeaf
+          ? ['node-view', { 'data-html': html }]
+          : ['node-view', { 'data-html': html }, ['node-view-content-editable', 0]];
       }
     },
 
--- packages/ui/src/tiptap/lib/html.ts
+++ packages/ui/src/tiptap/lib/html.ts
@@ -1,20 +1,29 @@
 import { generateHTML } from '@tiptap/html';
-import { handleHTML } from 'zeed-dom';
+import { handleHTML, parseHTML } from 'zeed-dom';
 import type { Extensions, JSONContent } from '@tiptap/core';
 
 export const renderHTML = (content: JSONContent, extensions: Extensions) => {
   const html = generateHTML(content, extensions);
 
   return handleHTML(html, (document) => {
-    const nodeViews = document.querySelectorAll('node-view');
-    for (const nodeView of nodeViews) {
-      const contentEditable = nodeView.querySelector('node-view-content-editable');
-      const hole = nodeView.querySelector('[data-node-view-content-editable]');
-      if (!contentEditable || !hole) {
+    const nodeViewWrappers = document.querySelectorAll('node-view');
+
+    for (const nodeViewWrapper of nodeViewWrappers) {
+      const nodeViewContent = parseHTML(nodeViewWrapper.dataset.html ?? '');
+      const nodeView = nodeViewContent.querySelector('[data-node-view]');
+
+      if (!nodeView) {
         continue;
       }
 
-      hole.replaceWith(contentEditable);
+      const nodeViewContentEditableWrapper = nodeViewWrapper.querySelector('node-view-content-editable');
+      const nodeViewContentEditable = nodeView.querySelector('[data-node-view-content-editable]');
+
+      if (nodeViewContentEditableWrapper && nodeViewContentEditable) {
+        nodeViewContentEditable.replaceChildren(...nodeViewContentEditableWrapper.children);
+      }
+
+      nodeViewWrapper.replaceWith(nodeView);
     }
   });
 };
