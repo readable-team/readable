f9d71d856348ec9445d13cc45453098e36892612
devunt <337728+devunt@users.noreply.github.com>
2024-10-19 01:49:32 +0000

    페이지 코멘트 팝오버 디자인 적용 (#1283)
    ![CleanShot 2024-10-19 at 10 43 50](https://github.com/user-attachments/assets/c74c04dc-d715-45b9-9c0b-ca82cc87120b)

--- apps/dashboard/src/routes/(default)/[teamId]/[siteId]/(page)/[pageId]/@menus/CommentPopover.svelte
+++ apps/dashboard/src/routes/(default)/[teamId]/[siteId]/(page)/[pageId]/@menus/CommentPopover.svelte
@@ -3,9 +3,11 @@
   import { center, flex } from '@readable/styled-system/patterns';
   import { createFloatingActions } from '@readable/ui/actions';
   import { Icon } from '@readable/ui/components';
+  import dayjs from 'dayjs';
   import { createEventDispatcher, onMount } from 'svelte';
   import ArrowUpIcon from '~icons/lucide/arrow-up';
   import { fragment, graphql } from '$graphql';
+  import { Img } from '$lib/components';
   import type { Editor } from '@tiptap/core';
   import type { Editor_CommentPopover_pageContentComment } from '$graphql';
 
@@ -22,8 +24,18 @@
     graphql(`
       fragment Editor_CommentPopover_pageContentComment on PageContentComment {
         id
-        nodeId
         content
+        createdAt
+
+        user {
+          id
+          name
+
+          avatar {
+            id
+            ...Img_image
+          }
+        }
       }
     `),
   );
@@ -83,36 +95,54 @@
   bind:this={popoverEl}
   class={flex({
     flexDirection: 'column',
-    gap: '8px',
+    gap: '12px',
     borderRadius: '4px',
     paddingX: '16px',
-    paddingY: '8px',
+    paddingY: '12px',
+    width: '320px',
+    maxHeight: '320px',
     backgroundColor: 'white',
     boxShadow: 'heavy',
     zIndex: '50',
   })}
   use:floating
 >
-  {#each $comments as comment (comment.id)}
-    <div>
-      {comment.content}
-    </div>
-  {/each}
+  <div class={flex({ flexGrow: '1', flexDirection: 'column', gap: '12px', overflowY: 'auto' })}>
+    {#each $comments as comment (comment.id)}
+      <div class={flex({ align: 'flex-start', gap: '8px' })}>
+        <Img
+          style={css.raw({ flex: 'none', borderRadius: 'full', size: '32px' })}
+          $image={comment.user.avatar}
+          alt={comment.user.name}
+          size={32}
+        />
+        <div class={flex({ flexGrow: '1', flexDirection: 'column' })}>
+          <div class={flex({ align: 'center', gap: '4px' })}>
+            <div class={css({ flexGrow: '1', textStyle: '13b' })}>{comment.user.name}</div>
+            <div class={css({ textStyle: '12r', color: 'neutral.50' })}>{dayjs(comment.createdAt).fromNow()}</div>
+          </div>
+          <div class={css({ textStyle: '14r' })}>{comment.content}</div>
+        </div>
+      </div>
+    {/each}
+  </div>
 
   <div class={flex({ gap: '8px' })}>
     <input
       class={css({
+        flexGrow: '1',
         borderWidth: '1px',
         borderRadius: '4px',
         paddingX: '8px',
         paddingY: '4px',
+        textStyle: '14r',
         backgroundColor: 'neutral.20',
       })}
       type="text"
       bind:value={content}
     />
     <button
-      class={center({ borderRadius: 'full', size: '32px', color: 'white', backgroundColor: 'accent.50' })}
+      class={center({ flex: 'none', borderRadius: 'full', size: '32px', color: 'white', backgroundColor: 'accent.50' })}
       type="button"
       on:click={onSubmit}
     >
