46e3fd916f1ce675f7b0a709daa428f206ce719e
robin-maki <8201350+robin-maki@users.noreply.github.com>
2024-10-21 08:02:50 +0000

    플랜 규칙 체크 함수에 로더 사용 (#1292)

--- apps/api/src/utils/plan.ts
+++ apps/api/src/utils/plan.ts
@@ -1,26 +1,46 @@
-import { and, count, eq } from 'drizzle-orm';
+import { and, count, eq, getTableColumns, inArray } from 'drizzle-orm';
 import { db, firstOrThrow, Plans, Sites, TeamMembers, TeamPlans } from '@/db';
 import { defaultPlanRules } from '@/db/schemas/json';
 import { SiteState } from '@/enums';
 import { ReadableError } from '@/errors';
+import type { Context } from '@/context';
 import type { PlanRules } from '@/db/schemas/json';
 
 type GetPlanParams<T extends keyof PlanRules> = {
   teamId: string;
   rule: T;
+  ctx?: Context;
 };
 
 export const getTeamPlanRule = async <T extends keyof PlanRules>({
   teamId,
   rule,
+  ctx,
 }: GetPlanParams<T>): Promise<PlanRules[T]> => {
-  const plan = await db
-    .select({ rules: Plans.rules })
-    .from(Plans)
-    .innerJoin(TeamPlans, eq(TeamPlans.planId, Plans.id))
-    .where(eq(TeamPlans.teamId, teamId))
-    .then(firstOrThrow);
+  const plan = await (async () => {
+    if (ctx) {
+      const planLoader = ctx.loader({
+        name: 'Plans&TeamPlans.teamId(teamId)',
+        load: async (teamIds: string[]) => {
+          return await db
+            .select({ ...getTableColumns(Plans), teamId: TeamPlans.teamId })
+            .from(TeamPlans)
+            .innerJoin(Plans, eq(TeamPlans.planId, Plans.id))
+            .where(inArray(TeamPlans.teamId, teamIds));
+        },
+        key: (row) => row.teamId,
+      });
 
+      return await planLoader.load(teamId);
+    } else {
+      return await db
+        .select({ rules: Plans.rules })
+        .from(Plans)
+        .innerJoin(TeamPlans, eq(TeamPlans.planId, Plans.id))
+        .where(eq(TeamPlans.teamId, teamId))
+        .then(firstOrThrow);
+    }
+  })();
   return plan.rules[rule] === undefined ? defaultPlanRules[rule] : plan.rules[rule];
 };
 
